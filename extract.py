a7175ff50b2ffd664a4b4134f37d699941722151
Andrey Labanca <moraislabanca@gmail.com>
2023-05-18 17:35:30 -0300

    cria pastas e salva datapackages e resources

---
+++ extract.py
@@ -0,0 +1,69 @@
+import requests
+import shutil
+import os
+import mimetypes
+from urllib.parse import urlparse
+from frictionless import Package, Resource
+from transform import merge_remove_path_redundancies
+from pathlib import Path
+
+# TODO replace os package for pathlib
+
+def save_source(source):
+
+    dataset_dir = 'datapackages'
+    file_dir = os.path.join(dataset_dir, source['name'])
+
+    os.makedirs(file_dir, exist_ok=True)
+
+    file_path = os.path.join(file_dir, 'datapackage.json')
+
+    dp_source = Package(source['path'])
+    dp_source.to_json(file_path) # Done!
+
+    # TODO : 1. para cada resource de cada source, salvar os arquivos de dados na pasta do datapackage obedecendo a estrutura relativa
+
+    for resource in dp_source.resources:
+
+        url = resource.path
+        parsed_url = urlparse(url)
+        print(parsed_url.path)
+        print(parsed_url.scheme)
+
+        #if resource.remote == True:
+        if parsed_url.scheme == 'https':
+            # TODO code for https url
+            print('NEED IMPLEMENTATION')
+
+        #elif resource.remote == False:
+        elif parsed_url.scheme == '':
+            resource_remotepath = Path(resource.basepath, resource.path)
+            response = requests.get(resource_path, stream=True)
+            response.raise_for_status()
+
+            datapackage_path = parsed_url.path.lstrip('/') # TODO change name, not always a dp
+
+            url_dir = os.path.join(source['name'], os.path.dirname(datapackage_path))
+
+            dataset_dir = 'datapackages' # TODO not put string literals inside a function. Parameter, constant or arg?
+            file_dir = os.path.join(dataset_dir, url_dir)
+
+            os.makedirs(file_dir, exist_ok=True)
+            file_path = merge_remove_path_redundancies(file_dir, datapackage_path)
+            #file_path = os.path.join(file_dir, datapackage_path) # if above works, remove this
+
+
+            #if mimetypes.guess_type(file_path)[0] in ('text/plain', 'text/csv'):
+            if 'text' in resource.mediatype:
+                response.raw.decode_content = True
+            else:
+                response.raw.decode_content = False
+
+
+            with open(file_path, 'wb') as file:
+                shutil.copyfileobj(response.raw, file)
+
+    # TODO : 2. tratar depois os casos que o path do resouce de uma source é uma URL.
+    # TODO : 3. tratar situação de quando a source do datapackge_master for um arquivo local (frictionless.byte_stream() deve tratar os dois casos melhor.
+    # TODO : 4. Como tratar um data_resource que não tem schema?
+
