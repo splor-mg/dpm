ebc9966394a12aa17cadd88764b93ac8e68fbc2a
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-26 11:05:18 -0300

    adiciona comando cli dpm install

--- src/dpm/load.py
+++ src/dpm/load.py
@@ -1,6 +1,19 @@
+import typer
 from frictionless import Package
 from dpm.extract import extract_sources
 
-def main():
-    dp_spreadmart = Package('datapackage.json')
-    extract_sources(dp_spreadmart)
+app = typer.Typer()
+
+@app.callback()
+def callback():
+    """
+    Data package manager to install, update and remove data dependencies.
+    """
+
+@app.command()
+def install(descriptor: str = 'datapackage.yaml'):
+    """
+    Download data packages (descriptor and resources data files) listed in package.sources and saves into datapackages/
+    """
+    package = Package(descriptor)
+    extract_sources(package)
