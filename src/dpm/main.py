9b712861708c4447d275c3e0d03214fec69686e2
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-05-26 10:09:38 -0300

    remove arquivos vazios e/ou desnecessarios

--- src/dpm/main.py
+++
@@ -1,9 +0,0 @@
-from load import main
-import logging
-if __name__ == '__main__':
-    LOG_FORMAT = '%(asctime)s %(levelname)-5.5s [%(name)s] %(message)s'
-    LOG_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
-    logging.basicConfig(format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT, level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    main()
