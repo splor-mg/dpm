889a4118dfef2d84c2c4f68612004819691fe88c
labanca <moraislabanca@gmail.com>
2023-06-06 19:02:35 -0300

    acesso a repos privados do github com token de autenticação.

--- src/dpm/install.py
+++ src/dpm/install.py
@@ -1,14 +1,22 @@
 import requests
 import shutil
-from frictionless import Package
-from pathlib import Path
 import logging
 import os
-
-key = 'GITHUB_TOKEN'
-github_token = os.getenv(key)
+from frictionless import Package, system
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
+
+def update_session_headers(session, source):
+
+    token = source.get('token')
+
+    if token:
+        session.headers['Authorization'] = f"Bearer {token}"
+        return session
+    else:
+        return session
+
 
 def extract_source_packages(package):
     for source in package.sources:
@@ -16,17 +24,24 @@ def extract_source_packages(package):
         extract_source_package(source)
 
 def extract_source_package(source):
-    package = Package(source['path'])
+
+
+    session = requests.Session()
+    # create a session and get the token if needed to auth a private repo
+    session = update_session_headers(session, source)
+
+    with system.use_context(http_session=session):
+        package = Package(source['path'] )
+
     package_descriptor_path = Path('datapackages', source['name'], 'datapackage.json')
     package.dereference()
     package.to_json(package_descriptor_path)
 
+
     for resource in package.resources:
         resource_remotepath = f'{resource.basepath}/{resource.path}'
-        headers = {'Authorization': f'token {github_token}'}
-        response = requests.get(str(resource_remotepath), headers=headers, stream=True)
-        #response = requests.get(str(resource_remotepath), stream=True)
 
+        response = session.get(str(resource_remotepath), stream=True)
         response.raise_for_status()
 
         resource_path = Path(package_descriptor_path.parent, resource.path)
@@ -41,3 +56,4 @@ def extract_source_package(source):
             shutil.copyfileobj(response.raw, file)
 
         logger.info(f'Data file of resource {resource.name} saved in {resource_path}')
+
