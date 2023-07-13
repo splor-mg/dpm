59923d8ecb1d7bf0338a1f0eb7bce383aa2c5419
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-07-13 09:38:56 -0300

    add NA as missingValue for uo_desc field

--- tests/unit/test_steps.py
+++ tests/unit/test_steps.py
@@ -13,7 +13,7 @@ def test_step_field_rename_to_target():
     resource.transform(pipeline)
     assert resource.schema.to_descriptor()['fields'] == [
             {'name': 'uo_cod', 'type': 'integer', 'constraints': {'required': True}, 'source': 'Unidade Orçamentária - Código'},
-            {'name': 'uo_desc', 'type': 'string', 'source': 'uo_desc'},
+            {'name': 'uo_desc', 'type': 'string', 'missingValues': ['NA'] , 'source': 'uo_desc'},
             {'name': 'valid', 'type': 'boolean', 'trueValues': ['Sim'], 'falseValues': ['Não'], 'source': 'Vigente?'},
             {'name': 'valid_from', 'type': 'date', 'format': '%d/%m/%Y', 'source': 'VALID_FROM'},
             {'name': 'valid_to', 'type': 'date', 'description': 'This is a big and nice description\n', 'format': '%d/%m/%Y', 'source': 'Valid_to'},
