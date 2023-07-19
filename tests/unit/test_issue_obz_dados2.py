000a6ff41ec78650f3c5cf21f7d032c8bbacbb2a
Andrey Morais Labanca SEPLAG <m7525850@ca.mg.gov.br>
2023-07-19 18:16:52 -0300

    adiciona reprex para issue splor-mg/obz-dados#2

---
+++ tests/unit/test_issue_obz_dados2.py
@@ -0,0 +1,17 @@
+from frictionless import Resource, Schema, Pipeline, steps
+from frictionless.resources import TableResource
+from dpm.steps import field_rename_to_target, table_write_normalized
+from datetime import date, datetime
+from pathlib import Path
+
+def test_step_field_rename_to_target():
+    resource = Resource('tests/data/temporal-dim.yaml')
+    
+    pipeline = Pipeline(
+        steps=[
+            field_rename_to_target(),
+            steps.row_filter(formula='uo_cod != 1251'),
+        ],
+    )
+    resource.transform(pipeline)
+    assert len(resource.read_rows()) == 2
