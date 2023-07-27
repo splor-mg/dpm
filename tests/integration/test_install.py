9a618f978a880a9606b6d524902a605c5fc9a6d8
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-07-27 19:21:10 -0300

    update tests

--- tests/integration/test_install.py
+++ tests/integration/test_install.py
@@ -1,20 +1,19 @@
 from typer.testing import CliRunner
 from dpm.cli import app
-from pathlib import Path
 
 runner = CliRunner()
 
-def test_app():
-    result = runner.invoke(app, ["install", "--descriptor", "data/datapackage.yaml"])
-    expected = ['datapackages/reprex-excel', 
-                'datapackages/transparencia-reprex-foreignkey', 
-                'datapackages/reprex-excel/missing_date.xlsx', 
-                'datapackages/reprex-excel/datapackage.json', 
-                'datapackages/reprex-excel/missing_date.csv', 
-                'datapackages/transparencia-reprex-foreignkey/datapackage.json', 
-                'datapackages/transparencia-reprex-foreignkey/data', 
-                'datapackages/transparencia-reprex-foreignkey/data/estados.csv', 
-                'datapackages/transparencia-reprex-foreignkey/data/pib-per-capita.csv']
-    result = [str(file) for file in Path('datapackages').glob('**/*')]
-    
-    assert expected == result
+def test_app(tmp_path):
+        result = runner.invoke(app, ['install', 'tests/data/data.yaml', '--output-dir', str(tmp_path)])
+        expected_files = [f'{str(tmp_path)}/reprex-excel', 
+                          f'{str(tmp_path)}/transparencia-reprex-foreignkey', 
+                          f'{str(tmp_path)}/reprex-excel/missing_date.xlsx', 
+                          f'{str(tmp_path)}/reprex-excel/datapackage.json', 
+                          f'{str(tmp_path)}/reprex-excel/missing_date.csv', 
+                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/datapackage.json', 
+                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data', 
+                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data/estados.csv', 
+                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data/pib-per-capita.csv']
+        files = [str(file) for file in tmp_path.glob('**/*')]
+        assert result.exit_code == 0
+        assert expected_files == files
