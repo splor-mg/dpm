a7fbb94ea67c49b51113604973888118eaa027b7
labanca <moraislabanca@gmail.com>
2023-10-05 19:30:03 -0300

    atualiza o test test_install.py para descriptors .toml

--- tests/integration/test_install.py
+++ tests/integration/test_install.py
@@ -1,19 +1,23 @@
 from typer.testing import CliRunner
 from dpm.cli import app
 
+
 runner = CliRunner()
 
 def test_app(tmp_path):
         result = runner.invoke(app, ['install', 'tests/data/data.toml', '--output-dir', str(tmp_path)])
-        expected_files = [f'{str(tmp_path)}/reprex-excel', 
-                          f'{str(tmp_path)}/transparencia-reprex-foreignkey', 
-                          f'{str(tmp_path)}/reprex-excel/missing_date.xlsx', 
-                          f'{str(tmp_path)}/reprex-excel/datapackage.json', 
-                          f'{str(tmp_path)}/reprex-excel/missing_date.csv', 
-                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/datapackage.json', 
-                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data', 
-                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data/estados.csv', 
-                          f'{str(tmp_path)}/transparencia-reprex-foreignkey/data/pib-per-capita.csv']
-        files = [str(file) for file in tmp_path.glob('**/*')]
+        expected_files = sorted([
+                                tmp_path / 'reprex-excel',
+                                tmp_path / 'transparencia-reprex-foreignkey',
+                                tmp_path / 'reprex-excel/missing_date.xlsx',
+                                tmp_path / 'reprex-excel/datapackage.json',
+                                tmp_path / 'reprex-excel/missing_date.csv',
+                                tmp_path / 'transparencia-reprex-foreignkey/datapackage.json',
+                                tmp_path / 'transparencia-reprex-foreignkey/data',
+                                tmp_path / 'transparencia-reprex-foreignkey/data/estados.csv',
+                                tmp_path / 'transparencia-reprex-foreignkey/data/pib-per-capita.csv'])
+
+        files = sorted(tmp_path.glob('**/*'))
+
         assert result.exit_code == 0
         assert expected_files == files
