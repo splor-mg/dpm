1b0c923440c6940f55e36ce51f1cdd8809244ca7
labanca <moraislabanca@gmail.com>
2023-10-11 01:43:12 -0300

    test_fetch_resources funcional

--- tests/integration/test_fetch_resources.py
+++ tests/integration/test_fetch_resources.py
@@ -1,17 +1,19 @@
 from typer.testing import CliRunner
 from dpm.cli import app
+from pathlib import Path
 
 
 runner = CliRunner()
 
 def test_app(tmp_path):
-        result = runner.invoke(app, ['install', 'tests/data/fetch_resources.toml', '--output-dir', str(tmp_path)])
+        result = runner.invoke(app, ['install', Path('tests/data/fetch_resources.toml').as_posix(), '--output-dir', str(tmp_path)])
         expected_files = sorted([
                                 tmp_path / 'obz-dados/data/misc/aux_agrupamento.xlsx',
                                 tmp_path / 'obz-dados/data-raw/base_parecer_obz_dcmefo.xlsx',
                                 tmp_path / 'obz-dados/data-raw/execucao_painel_obz.xlsx',
                                 tmp_path / 'obz-dados/data-raw/uo_setor.xlsx',
                                 tmp_path / 'obz-dados/datapackage.json',
+                                tmp_path / 'obz-dados/obz-dados.xlsx',
                                 tmp_path / 'reestimativa-dados/data/reest_rec.csv',
                                 tmp_path / 'reestimativa-dados/datapackage.json',
                                 tmp_path / 'sisor-dados-2024/data/base_categoria_pessoal.csv',
@@ -19,7 +21,7 @@ def test_app(tmp_path):
                                 tmp_path / 'sisor-dados-2024/datapackage.json',
                                 tmp_path / 'teto-gastos-rrf-dados/datapackage.json'])
 
-        files = sorted(tmp_path.glob('**/*'))
+        files = sorted(tmp_path.glob('**/*.*'))
 
         assert result.exit_code == 0
         assert expected_files == files
