41fe69062f56cdccb551dfa7379896cd03362f1e
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-10-20 17:11:19 -0300

    permite instalacao de um unico data package via cli

--- src/dpm/cli.py
+++ src/dpm/cli.py
@@ -2,12 +2,15 @@ import typer
 from typing_extensions import Annotated
 from frictionless import Package
 from pathlib import Path
+import logging
 try:
     import tomllib
 except ModuleNotFoundError:
     import tomli as tomllib
 from dpm.install import extract_source_packages
 from dpm.load import read_manifest, load_package, create_if_not_exists_control_table
+
+logger = logging.getLogger(__name__)
 
 app = typer.Typer()
 
@@ -29,15 +32,23 @@ def install(descriptor: Annotated[Path, typer.Argument()] = Path('data.toml'),
     extract_source_packages(data_toml, output_dir)
 
 @app.command()
-def load():
+def load(manifest: Annotated[Path, typer.Argument()] = Path('data.toml'),
+         package: Annotated[Path, typer.Option()] = None
+         ):
     """
     Load data packages into database
     """
     create_if_not_exists_control_table()
     
-    manifest = read_manifest('data.toml')
-
-    for pkg_id in manifest['packages'].keys():
-        path = Path('datapackages') / pkg_id / 'datapackage.json'
-        package = Package(path)
-        load_package(pkg_id, package)
+    if package:
+        dp = Package(package)
+        if not dp.name:
+            logger.error(f"Package '{package}' is missing name. Aborting database load.")
+        else:
+            load_package(dp.name, dp)
+    else:
+        manifest = read_manifest('data.toml')
+        for pkg_id in manifest['packages'].keys():
+            path = Path('datapackages') / pkg_id / 'datapackage.json'
+            package = Package(path)
+            load_package(pkg_id, package)
--- src/dpm/db.py
+++ src/dpm/db.py
@@ -1,6 +1,12 @@
 import os
+import logging
 import sqlalchemy as sa
+
+logger = logging.getLogger(__name__)
 
 conn_string = os.getenv('SQLALCHEMY_URL') # or maybe DB_CONNECTION_STRING?
 if conn_string:
     engine = sa.create_engine(conn_string)
+else:
+    logger.info('SQLALCHEMY_URL environment variable not found. dpm load will not work.')
+    engine = None
