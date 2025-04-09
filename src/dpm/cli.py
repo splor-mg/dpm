a0703bec319ef03102705d29ddbe5ee5e8c6edfa
labanca <moraislabanca@gmail.com>
2025-04-09 16:49:34 -0300

    chore(concat): atualiza help do opção --enrich do comando concat.

--- src/dpm/cli.py
+++ src/dpm/cli.py
@@ -100,7 +100,7 @@ def cli_concat(
         typer.Option(
             "--enrich",
             "-e",
-            help="Create a new column named 'name' with value from the data package property 'key'",
+            help="use `name:key` pattern to create a new column named 'name' with value from the data package property named 'key'",
             callback=_validate_enrich_option
         ),
     ] = [],
